124ef4b0f6a3b10e3b638d15be5b54cbcefaaa1f
playersharkshasha <playersharkshasha@gmail.com>
2026-02-06 12:17:09 +0800

    Add for Lab3 reuqirements

--- chatbot.py
+++ chatbot.py
@@ -3,6 +3,8 @@ This program requires the following modules:
 - python-telegram-bot==22.5
 - urllib3==2.6.2
 '''
+from ChatGPT_HKBU import ChatGPT
+gpt = None
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 import configparser
@@ -22,6 +24,10 @@ def main():
     logging.info('INIT: Connecting the Telegram bot...')
     app = ApplicationBuilder().token(config['TELEGRAM']['ACCESS_TOKEN']).build()
 
+    # Create a ChatGPT client object
+    global gpt
+    gpt = ChatGPT(config)
+
     # Register a message handler
     logging.info('INIT: Registering the message handler...')
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, callback))
@@ -31,11 +37,15 @@ def main():
     app.run_polling()
 
 async def callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    # await update.message.reply_text(response)
     logging.info("UPDATE: " + str(update))
+    loading_message = await update.message.reply_text('Thinking...')
 
-    # send the echo back to the client
-    text = update.message.text.upper()
-    await update.message.reply_text(text)
+    # send the user message to the ChatGPT client
+    response = gpt.submit(update.message.text)
+
+    # send the response to the Telegram box client
+    await loading_message.edit_text(response)
 
 if __name__ == '__main__':
     main()
